35f63f352bab05e8610e9f39bf2da1467a2fe5f5
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-07-28 13:30:58 +0300

    draft: issue contract

---
+++ src/Laraue.Apps.Boards.DataAccess/Models/IssueChange.cs
@@ -0,0 +1,12 @@
+﻿namespace Laraue.Apps.Boards.DataAccess.Models;
+
+public class IssueChange
+{
+    public long Id { get; set; }
+    
+    public long IssueId { get; set; }
+    public Issue? Issue { get; set; }
+    
+    public string? OldValue { get; set; }
+    public string? NewValue { get; set; }
+}
